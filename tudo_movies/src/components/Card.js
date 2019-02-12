1bf7cf64f71ee970912c9c6f052812a4d2499f42
FagnerSI <fagner314@gmail.com>
2019-02-12 12:19:42 -0300

    Requicição de pagina realizada por scroll

--- tudo_movies/src/components/Card.js
+++ tudo_movies/src/components/Card.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import './Card.css';
 
 
+import axios from 'axios';
+
 export default class Card extends Component {
+
+
+  state = {
+    movie: ''
+  }
+
+  componentDidMount() {
+    const { movie } = this.props;
+    const API_KEY = '7fa47550a2cf9a4e94701d5e5940ce12';
+
+
+    axios.get('https://api.themoviedb.org/3/movie/' + movie.id + '?api_key=' + API_KEY + '&language=pt-BR')
+      .then(response => {
+        const movie = response.data;
+        this.setState({ movie });
+      });
+  }
 
   render() {
 
-    const { movie } = this.props;
+    const { movie } = this.state;
     const base_url = 'https://image.tmdb.org/t/p/';
     const size = 'w780';
 
@@ -26,20 +45,20 @@ export default class Card extends Component {
 
 
     return (
-      <div className="card">
-        <img className="img-back" src={base_url + size + movie.backdrop_path}
-          alt={'Imagem do filme: ' + movie.title} />
+      <div className="card" >
+        <img className="img-back" src={base_url + size + movie.backdrop_path} alt="" />
 
-        <img className="img-min" src={base_url + size + movie.poster_path}
-          alt={'Poster do filme: ' + movie.title} />
+        <img className="img-min" src={base_url + size + movie.poster_path} alt="" />
 
         <div className="card-body">
+
           <div className={classRanger}>{movie.vote_average}</div>
           <h3 className="movie-title"> {movie.title} </h3>
-          <p className="movie-desc">{movie.overview}</p>
+          <p className="movie-desc">{movie.tagline}</p>
           <span className="movie-date">
             {movie.release_date}
           </span>
+          
         </div>
       </div>
 
--- tudo_movies/src/components/Container.js
+++ tudo_movies/src/components/Container.js
@@ -6,32 +6,48 @@ import Card from './Card'
 import axios from 'axios';
 
 export default class Container extends Component {
-    
+
+    //https://api.themoviedb.org/3/search/movie?query=${velozes%20e%20furiosos}&api_key=7fa47550a2cf9a4e94701d5e5940ce12&languagem=pt-BR
+   
     state = {
-        movies: []
+        movies: [],
+        page: 1,
+    }
+    componentDidMount() {    
+        this.handleScroll();        
+        window.onscroll = () => this.handleScroll();
     }
 
-   componentDidMount() {
-        const myKey = '7fa47550a2cf9a4e94701d5e5940ce12';
+    handleScroll = (e) => {
+       
+        if (window.pageYOffset + window.innerHeight >= document.body.offsetHeight - 400) {
 
-        axios.get('https://api.themoviedb.org/3/movie/popular?api_key='+myKey+'&language=pt-BR&page=1')
+            const { page } = this.state;
+            const API_KEY = '7fa47550a2cf9a4e94701d5e5940ce12';
+            axios.get('https://api.themoviedb.org/3/movie/popular?api_key=' + API_KEY + '&language=pt-BR&page=' + page)
             .then(response => {
                 const movies = response.data.results;
-                console.log(movies)
-                this.setState({ movies });
+                this.setState({
+                    movies: [...this.state.movies, ...movies],
+                    page: page + 1,
+                });
+                
             });
+            
+        }
+       
     }
 
     render() {
-        return (
-            <div className="content">
-                <h2 className="title">Recentes</h2>
+            return (
+                <div className="content">
+                    <h2 className="title">Recentes</h2>
 
-                {this.state.movies.map( movie => (
-                    <Card key={ movie.id } movie={ movie }/>
-                ))}
-                
-            </div>
-        );
+                    {this.state.movies.map(movie => (
+                        <Card key={movie.id} movie={movie} />
+                    ))}
+
+                </div>
+            );
+        }
     }
-}
