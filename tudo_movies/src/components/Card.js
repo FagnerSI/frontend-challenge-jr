cc27d1c9e12e416b99416e66901176a71f103bac
FagnerSI <fagner314@gmail.com>
2019-02-13 16:44:09 -0300

    Ajuste no carregamento de paginas de filmes

--- tudo_movies/src/components/Card.js
+++ tudo_movies/src/components/Card.js
@@ -1,22 +1,20 @@
 import React, { Component } from 'react';
 import '../css/Card.css';
 
-
 import axios from 'axios';
 
 export default class Card extends Component {
-
 
   state = {
     movie: ''
   }
 
   componentDidMount() {
+    
     const { movie } = this.props;
     const API_KEY = '7fa47550a2cf9a4e94701d5e5940ce12';
 
-
-    axios.get('https://api.themoviedb.org/3/movie/' + movie.id + '?api_key=' + API_KEY + '&language=pt-BR')
+     axios.get('https://api.themoviedb.org/3/movie/' + movie.id + '?api_key=' + API_KEY + '&language=pt-BR')
       .then(response => {
         const movie = response.data;
         this.setState({ movie });
@@ -46,9 +44,9 @@ export default class Card extends Component {
 
     return (
       <div className="card" >
-        <img className="img-back" src={base_url + size + movie.backdrop_path} alt={'Imagem do filme: '+ movie.title}/>
+        <img className="img-back" src={base_url + size + movie.backdrop_path} alt={'Imagem: '+ movie.title}/>
 
-        <img className="img-min" src={base_url + size + movie.poster_path} alt={'Poster do filme: '+ movie.title}/>
+        <img className="img-min" src={base_url + size + movie.poster_path} alt={'Poster: '+ movie.title}/>
 
         <div className="card-body">
 
--- tudo_movies/src/components/Container.js
+++ tudo_movies/src/components/Container.js
@@ -7,56 +7,81 @@ import axios from 'axios';
 
 export default class Container extends Component {
 
-    //https://api.themoviedb.org/3/search/movie?query=${velozes%20e%20furiosos}&api_key=7fa47550a2cf9a4e94701d5e5940ce12&languagem=pt-BR
-   
     state = {
         movies: [],
+        total_results: 0,
+        total_pages: 1,
         page: 1,
-        search: false
+        search: ''
     }
-    componentDidMount() {    
+
+    componentDidMount() {
         this.handleScroll();
         window.onscroll = () => this.handleScroll();
     }
 
-    componentWillMount(){
-        Pubsub.subscribe('searchedMovie', (topic, movies) => {
-            this.setState({                
-                movies
+    componentWillMount() {
+        Pubsub.subscribe('searchedMovieResult', (topic, search) => {
+            this.setState({
+                movies: [],
+                page: 1,
+                search
             })
+            this.loadData();
         })
     }
 
     handleScroll = (e) => {
-       
         if (window.pageYOffset + window.innerHeight >= document.body.offsetHeight - 400) {
-
-            const { page } = this.state;
-            const API_KEY = '7fa47550a2cf9a4e94701d5e5940ce12';
-            axios.get('https://api.themoviedb.org/3/movie/popular?api_key=' + API_KEY + '&language=pt-BR&page=' + page)
-            .then(response => {
-                const movies = response.data.results;
-                this.setState({
-                    movies: [...this.state.movies, ...movies],
-                    page: page + 1,
-                });
-                
-            });
-            
+            this.loadData();
         }
-       
+    }
+
+    loadData = () => {
+        const API_KEY = '7fa47550a2cf9a4e94701d5e5940ce12';
+        let { page } = this.state;
+        let { total_pages } = this.state;
+        let { search } = this.state;
+
+        let url = 'https://api.themoviedb.org/3/movie/popular?api_key=' + API_KEY + '&language=pt-BR&page=' + page;
+
+        if (search) {
+            url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&language=en-US&query=' + search + '&page=' + page;
+        }
+        
+        if (page <= total_pages) {
+            axios.get(url)
+                .then(response => {
+                    let movies = response.data;                    
+                    this.setState({
+                        movies: [...this.state.movies, ...movies.results],
+                        total_results: movies.total_results,
+                        total_pages: movies.total_pages,
+                        page: page + 1
+                    });
+
+                });            
+        }
     }
 
     render() {
-            return (
-                <div className="content">
-                    <h2 className="title">Recentes</h2>
 
-                    {this.state.movies.map(movie => (
-                        <Card key={movie.id} movie={movie} />
-                    ))}
+        let { search } = this.state;
+        let { total_results } = this.state;
+        let title = 'Recentes';
 
-                </div>
-            );
+        if (search) {
+            title = total_results + ' resultados para: ' + search;
         }
+        return (
+            <div className="content">
+                <h2 className="title">{title}</h2>
+
+                {this.state.movies.map(movie => (
+                    <Card key={movie.id} movie={movie} />
+                ))}
+
+            </div>
+        );
     }
+}
